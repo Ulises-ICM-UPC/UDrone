216ed903c168cc4c0168cdd0b868b43d9759bfda
Ulises <ulises@icm.csic.es>
2021-10-04 09:42:26 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -1,40 +1,54 @@
 '''
 Created on 2021 by Gonzalo Simarro and Daniel Calvete
 '''
+# Import modules
 import sys
 sys.path.insert(0, 'udrone')
-# own modules
 import udrone
 import os
 
-pathMain = 'example'
-pathFolderVideo = pathMain
-pathFolderFrames = pathMain + os.sep + 'frames'
+pathMain = 'example' # USER DEFINED (main folder)
 
-FPS = 2.00
-
+'''
+Video extraction
+'''
+pathFolderVideo = pathMain # USER DEFINED (folder where the video is located)
+pathFolderFrames = pathMain + os.sep + 'frames' # USER DEFINED (folder where the frames extractes fron the video wwil be placed)
+FPS = 2.00 # USER DEFINED (extraction rate of the frames )
 print('Video extraction')
 udrone.Video2Frames(pathFolderVideo, pathFolderFrames, FPS)
 
-pathFolderBasis = pathMain + os.sep + 'basis'
-calibrationModel = 'parabolic'
+'''
+Intrinsic calibration
+'''
+pathFolderBasis = pathMain + os.sep + 'basis' # USER DEFINED (path where files for calibrating the basis are located)
+calibrationModel = 'parabolic' # USER DEFINED (intrinsic camera calibration model [parabolic,quartic,full])
 print('Intrinsic calibration')
 print('Calibration of the basis')
 udrone.calibrationOfBasisImages(pathFolderBasis, calibrationModel)
 print('Optimal intrinsic parameters of the camera')
 udrone.calibrationOfBasisImagesConstantIntrinsic(pathFolderBasis, calibrationModel)
 
-verbosePlot = True
+'''
+Automatic calibration
+'''
+verbosePlot = True # USER DEFINED
 print('Calibration of the frames')
 udrone.autoCalibrationOfFrames(pathFolderBasis, pathFolderFrames, verbosePlot)
 
-pathFolderPlanv = pathMain + os.sep + 'planviews'
-z0 = 1.5
-ppm = 1.0
-verbosePlanviews = True
+'''
+Planviews
+'''
+z0 = 1.5 # USER DEFINED (height at which the projection is made)
+ppm = 1.0 # USER DEFINED (resolution of the planviews, pixels-per-meter)
+verbosePlanviews = True # USER DEFINED 
+pathFolderPlanv = pathMain + os.sep + 'planviews' # USER DEFINED (path where the planview images will be located)
 print('Generation of planviews')
 udrone.planviewsFromSnaps(pathMain, pathFolderFrames, pathFolderPlanv, z0, ppm, verbosePlanviews)
 
-pathBasisCheck = pathMain + os.sep + 'basis_check'
+'''
+GCP check
+'''
+pathBasisCheck = pathMain + os.sep + 'basis_check' # USER DEFINED (path where GCP-files to be checked are located)
 print('Check GCP for basis calibration')
 udrone.checkGCPs(pathBasisCheck)
