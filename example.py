31156dd1c76e2d16f8fea7ff720e918e846796f1
Ulises <ulises@icm.csic.es>
2022-03-10 17:18:46 +0100

    Add files via upload

--- example.py
+++ example.py
@@ -1,26 +1,15 @@
 #'''
-# Created on 2021 by Gonzalo Simarro, Daniel Calvete and Paola Souto
+# Created on 2022 by Gonzalo Simarro, Daniel Calvete and Paola Souto
 #'''
 #
-import sys
 import os
+import sys
 #
-sys.path.insert(0, 'udrone')
-import udrone as udrone
+sys.path.insert(0, 'ucalib')
+import ucalib as ucalib
 #
-pathFolderMain = 'example' # USER DEFINED example TMP_Cali_201908161521
+pathFolderMain = 'example' # USER DEFINED
 assert os.path.exists(pathFolderMain)
-#
-#''' --------------------------------------------------------------------------
-# Extraction of the video
-#''' --------------------------------------------------------------------------
-#
-pathFolderVideo = pathFolderMain # USER DEFINED (folder where the video is located)
-pathFolderFrames = pathFolderMain + os.sep + 'frames' # USER DEFINED (folder where the frames extractes fron the video wwil be placed)
-FPS = 2.00 # USER DEFINED (extraction rate of the frames )
-#
-print('Video extraction')
-udrone.Video2Frames(pathFolderVideo, pathFolderFrames, FPS)
 #
 #''' --------------------------------------------------------------------------
 # Calibration of the basis
@@ -31,62 +20,41 @@ eCritical, calibrationModel = 5., 'parabolic' # USER DEFINED (eCritical is in pi
 verbosePlot = True # USER DEFINED
 #
 print('Calibration of the basis')
-udrone.CalibrationOfBasisImages(pathFolderBasis, eCritical, calibrationModel, verbosePlot)
-print('Calibration of the basis forcing a unique intrinsic parameters')
-udrone.CalibrationOfBasisImagesConstantIntrinsic(pathFolderBasis, calibrationModel, verbosePlot)
+ucalib.CalibrationOfBasisImages(pathFolderBasis, eCritical, calibrationModel, verbosePlot)
+print('Calibration of the basis forcing a unique camera position and intrinsic parameters')
+ucalib.CalibrationOfBasisImagesConstantXYZAndIntrinsic(pathFolderBasis, calibrationModel, verbosePlot)
 #
 #''' --------------------------------------------------------------------------
-# (Auto)Calibration of the frames
+# (Auto)Calibration of the images
 #''' --------------------------------------------------------------------------
 #
-pathFolderBasis = pathFolderMain + os.sep + 'basis' # USER DEFINED
-pathFolderFrames = pathFolderMain + os.sep + 'frames' # USER DEFINED
+#pathFolderBasis = pathFolderMain + os.sep + 'basis' # USER DEFINED
+pathFolderImages = pathFolderMain + os.sep + 'images' # USER DEFINED
 verbosePlot = True # USER DEFINED
+nORB, fC, KC = 10000, 5., 4 # USER DEFINED
 #
-print('Autocalibration of the frames')
-udrone.AutoCalibrationOfFramesViaGCPs(pathFolderBasis, pathFolderFrames, verbosePlot)
+print('Autocalibration of the images')
+ucalib.AutoCalibrationOfImages(pathFolderBasis, pathFolderImages, nORB, fC, KC, verbosePlot)
 #
 #''' --------------------------------------------------------------------------
 # Plot planviews
 #''' --------------------------------------------------------------------------
 #
-#pathFolderFrames = pathFolderMain + os.sep + 'frames' # USER DEFINED
+#pathFolderImages = pathFolderMain + os.sep + 'images' # USER DEFINED
 pathFolderPlanviews = pathFolderMain + os.sep + 'planviews' # USER DEFINED
-z0, ppm = 3.2, 2.0 # USER DEFINED
+z0, ppm = 3.2, 1.0 # USER DEFINED
 verbosePlot = True # USER DEFINED
 #
 print('Generation of planviews')
-udrone.PlanviewsFromImages(pathFolderFrames, pathFolderPlanviews, z0, ppm, verbosePlot)
-#
-#''' --------------------------------------------------------------------------
-# Plot mean (timex) and sigma images of planviews
-#''' --------------------------------------------------------------------------
-#
-#pathFolderPlanviews = pathFolderMain + os.sep + 'planviews' # USER DEFINED
-#
-print('Generation of mean and sigma images for the planviews')
-udrone.TimexAngSigma(pathFolderPlanviews)
-#
-#''' --------------------------------------------------------------------------
-# Plot timestacks
-#''' --------------------------------------------------------------------------
-#
-#pathFolderFrames = pathFolderMain + os.sep + 'frames' # USER DEFINED
-pathFolderTimestack = pathFolderMain + os.sep + 'timestack' # USER DEFINED
-ppm = 10. # USER DEFINED
-includeNotCalibrated = True # USER DEFINED
-verbosePlot = True # USER DEFINED
-#
-print('Generation of timestack')
-udrone.TimestackFromImages(pathFolderFrames, pathFolderTimestack, ppm, includeNotCalibrated, verbosePlot)
+ucalib.PlanviewsFromImages(pathFolderImages, pathFolderPlanviews, z0, ppm, verbosePlot)
 #
 #''' --------------------------------------------------------------------------
 # check basis images
 #''' --------------------------------------------------------------------------
 #
 pathFolderBasisCheck = pathFolderMain + os.sep + 'basis_check' # USER DEFINED
-#eCritical, calibrationModel = 5., 'parabolic' # USER DEFINED (eCritical is in pixels, calibrationModel = 'parabolic', 'quartic' or 'full')
+#eCritical = 5. # USER DEFINED (eCritical is in pixels)
 #
 print('Checking of the basis')
-udrone.CheckGCPs(pathFolderBasisCheck, eCritical, calibrationModel)
+ucalib.CheckGCPs(pathFolderBasisCheck, eCritical)
 #
